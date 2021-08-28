97b545b9c15b8adfb926b9ad65285f44f1b3c100
George Wanjohi <george.wanjohi@thejitu.com>
2021-08-28 21:00:05 +0300

    COMPLETE TOUR OF HEROES

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,13 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id).subscribe(hero => this.hero = hero)
   }
 
+  save(){
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    } 
+  }
+
   goBack() {
     this.location.back();
   }
